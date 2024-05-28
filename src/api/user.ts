f7d5e2b3fb52d09a8a3952dadbc082da105f1018
wangxiaoyu006 <wangxiaoyu006@chinasofti.com>
2024-05-28 20:57:18 +0800

    auth/login接口

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,7 +3,7 @@ import http from '../utils/http'
 console.log(http, '---http---')
 export function userLogin(data: any) {
   console.log('1')
-  return http.post('/user/login', data)
+  return http.post('/auth/login', data)
   // return http.get('/book/list', data)
 }
 
